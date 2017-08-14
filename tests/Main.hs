4e916db3cd383eeafe379d718e7ebeffc32d9ba1
Henry Till <henrytill@gmail.com>
2017-08-13 20:56:44 -0400

    tests: add more cases

--- tests/Main.hs
+++ tests/Main.hs
@@ -38,14 +38,95 @@ listAt
   -> f (Map.Map T.Text TOML.Value)
 listAt k = at k . _Just . _List
 
-test1 :: [(T.Text, TOML.Value)] -> Test
-test1 kv = Expect "get key1 from array table" (==) expected actual
+testTableKey :: [(T.Text, TOML.Value)] -> Test
+testTableKey kv
+  = Expect "'key' from 'table' == Just \"value\""
+           (==) expected actual
+  where
+    expected = Just "value"
+    actual   = alist kv ^? mapAt "table" . at "key" . _Just . _String
+
+testTableZoo :: [(T.Text, TOML.Value)] -> Test
+testTableZoo kv
+  = Expect "'zoo' from 'table' == Nothing"
+           (==) expected actual
+  where
+    expected = Nothing
+    actual   = alist kv ^? mapAt "table" . at "zoo" . _Just . _String
+
+testTableSubtableKey :: [(T.Text, TOML.Value)] -> Test
+testTableSubtableKey kv
+  = Expect "'key' from 'subtable' from 'table' == Just \"another value\""
+           (==) expected actual
+  where
+    expected = Just "another value"
+    actual   = alist kv ^? mapAt "table" . mapAt "subtable" . at "key" . _Just . _String
+
+testTableInlineNameFirst :: [(T.Text, TOML.Value)] -> Test
+testTableInlineNameFirst kv
+  = Expect "'first' from 'name' from 'inline' from 'table' == \"Tom\""
+           (==) expected actual
+  where
+    expected = Just "Tom"
+    actual   = alist kv ^? mapAt "table" . mapAt "inline" . mapAt "name" . at "first" . _Just . _String
+
+testTableInlinePointY :: [(T.Text, TOML.Value)] -> Test
+testTableInlinePointY kv
+  = Expect "'y' from 'point' from 'inline' from 'table' == Just 2"
+           (==) expected actual
+  where
+    expected = Just 2
+    actual   = alist kv ^? mapAt "table" . mapAt "inline" . mapAt "point" . at "y" . _Just . _Integer
+
+testStringBasicBasic :: [(T.Text, TOML.Value)] -> Test
+testStringBasicBasic kv
+  = Expect "'basic' from 'basic' from 'string' == <some escaped nonsense>"
+           (==) expected actual
+  where
+    expected = Just "I'm a string. \"You can quote me\". Name\tJos\233\nLocation\tSF."
+    actual   = alist kv ^? mapAt "string" . mapAt "basic" . at "basic" . _Just . _String
+
+testStringMultiline :: [(T.Text, TOML.Value)] -> Test
+testStringMultiline kv
+  = Predicate "'key1', 'key2', and 'key3' from 'multiline' from 'string' are all the same"
+              (\ xs -> all (== head xs) (tail xs))
+              [actual1, actual2, actual3]
+  where
+    actual1 = alist kv ^? mapAt "string" . mapAt "multiline" . at "key1" . _Just . _String
+    actual2 = alist kv ^? mapAt "string" . mapAt "multiline" . at "key2" . _Just . _String
+    actual3 = alist kv ^? mapAt "string" . mapAt "multiline" . at "key3" . _Just . _String
+
+testStringMultilineContinued :: [(T.Text, TOML.Value)] -> Test
+testStringMultilineContinued kv
+  = Predicate "'key1', 'key2', and 'key3' from 'continued' from 'multiline' from 'string' are all the same"
+              (\ xs -> all (== head xs) (tail xs))
+              [actual1, actual2, actual3]
+  where
+    actual1 = alist kv ^? mapAt "string" . mapAt "multiline" . mapAt "continued" . at "key1" . _Just . _String
+    actual2 = alist kv ^? mapAt "string" . mapAt "multiline" . mapAt "continued" . at "key2" . _Just . _String
+    actual3 = alist kv ^? mapAt "string" . mapAt "multiline" . mapAt "continued" . at "key3" . _Just . _String
+
+testArrayKey1 :: [(T.Text, TOML.Value)] -> Test
+testArrayKey1 kv
+  = Expect "'key1' from 'array' == [1, 2, 3]"
+           (==) expected actual
   where
     expected = [1, 2, 3]
     actual   = alist kv ^.. mapAt "array" . listAt "key1" . traverse . _Integer
 
-runTests :: [(T.Text, TOML.Value)] -> IO [Result]
-runTests kv = pure (runTest <$> [test1 kv])
+runTests :: [(T.Text, TOML.Value)] -> [Result]
+runTests kv = runTest . ($ kv) <$> tests
+  where
+    tests = [ testTableKey
+            , testTableZoo
+            , testTableSubtableKey
+            , testTableInlineNameFirst
+            , testTableInlinePointY
+            , testStringBasicBasic
+            , testStringMultiline
+            , testStringMultilineContinued
+            , testArrayKey1
+            ]
 
 readTOMLFile :: String -> IO [(T.Text, TOML.Value)]
 readTOMLFile file = readFile file >>= parse >>= handleError
@@ -56,6 +137,6 @@ readTOMLFile file = readFile file >>= parse >>= handleError
 main :: IO ()
 main =  do
   ex <- readTOMLFile "./example/example-v0.4.0.toml"
-  rs <- runTests ex
+  rs <- pure (runTests ex)
   _  <- mapM_ print rs
   unless (all isPassed rs) exitFailure
