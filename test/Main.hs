63961bcf9be8f66f9bad987bbc2818d3264337ef
Eric Mertens <emertens@galois.com>
2024-02-27 13:26:00 -0800

    update tests

--- test/Main.hs
+++ test/Main.hs
@@ -1,13 +1,16 @@
+{-# Language OverloadedStrings #-}
 module Main (main) where
 
 import Control.Monad (unless)
+import qualified Data.Text.IO as Text
 import Lens.Family2
-import Lens.Family2.Stock (at, just_)
+import Lens.Family2.Stock (at, just_, _2)
 import System.Exit (exitFailure)
 import Test.Dwergaz
 import Toml (Table, Value)
 import qualified Toml
 import Toml.Lens
+import Data.Text (Text)
 
 allEqual :: (Eq a) => [a] -> Bool
 allEqual (x : xs) = all (== x) xs
@@ -15,19 +18,27 @@ allEqual [] = error "allEqual: empty list"
 
 mapAt ::
   (Applicative f) =>
-  String ->
+  Text ->
   (Table -> f Table) ->
   Table ->
   f Table
-mapAt k = at k . just_ . _Table
+mapAt k = valueAt k . _Table
 
 arrayAt ::
   (Applicative f) =>
-  String ->
+  Text ->
   ([Value] -> f [Value]) ->
   Table ->
   f Table
-arrayAt k = at k . just_ . _Array
+arrayAt k = valueAt k . _List
+
+valueAt ::
+  (Applicative f) =>
+  Text ->
+  (Value -> f Value) ->
+  Table ->
+  f Table
+valueAt k f (Toml.MkTable t) = Toml.MkTable <$> (at k . just_ . _2) f t
 
 testTableKey :: Table -> Test
 testTableKey kv =
@@ -38,7 +49,7 @@ testTableKey kv =
     actual
   where
     expected = Just "value"
-    actual = kv ^? mapAt "table" . at "key" . just_ . _String
+    actual = kv ^? mapAt "table" . valueAt "key" . _Text
 
 testTableZoo :: Table -> Test
 testTableZoo kv =
@@ -49,7 +60,7 @@ testTableZoo kv =
     actual
   where
     expected = Nothing
-    actual = kv ^? mapAt "table" . at "zoo" . just_ . _String
+    actual = kv ^? mapAt "table" . valueAt "zoo" . _Text
 
 testTableSubtableKey :: Table -> Test
 testTableSubtableKey kv =
@@ -60,7 +71,7 @@ testTableSubtableKey kv =
     actual
   where
     expected = Just "another value"
-    actual = kv ^? mapAt "table" . mapAt "subtable" . at "key" . just_ . _String
+    actual = kv ^? mapAt "table" . mapAt "subtable" . valueAt "key" . _Text
 
 testTableInlineNameFirst :: Table -> Test
 testTableInlineNameFirst kv =
@@ -71,7 +82,7 @@ testTableInlineNameFirst kv =
     actual
   where
     expected = Just "Tom"
-    actual = kv ^? mapAt "table" . mapAt "inline" . mapAt "name" . at "first" . just_ . _String
+    actual = kv ^? mapAt "table" . mapAt "inline" . mapAt "name" . valueAt "first" . _Text
 
 testTableInlinePointY :: Table -> Test
 testTableInlinePointY kv =
@@ -82,7 +93,7 @@ testTableInlinePointY kv =
     actual
   where
     expected = Just 2
-    actual = kv ^? mapAt "table" . mapAt "inline" . mapAt "point" . at "y" . just_ . _Integer
+    actual = kv ^? mapAt "table" . mapAt "inline" . mapAt "point" . valueAt "y" . _Integer
 
 testStringBasicBasic :: Table -> Test
 testStringBasicBasic kv =
@@ -93,7 +104,7 @@ testStringBasicBasic kv =
     actual
   where
     expected = Just "I'm a string. \"You can quote me\". Name\tJos\233\nLocation\tSF."
-    actual = kv ^? mapAt "string" . mapAt "basic" . at "basic" . just_ . _String
+    actual = kv ^? mapAt "string" . mapAt "basic" . valueAt "basic" . _Text
 
 testStringMultiline :: Table -> Test
 testStringMultiline kv =
@@ -102,9 +113,9 @@ testStringMultiline kv =
     allEqual
     [actual1, actual2, actual3]
   where
-    actual1 = kv ^? mapAt "string" . mapAt "multiline" . at "key1" . just_ . _String
-    actual2 = kv ^? mapAt "string" . mapAt "multiline" . at "key2" . just_ . _String
-    actual3 = kv ^? mapAt "string" . mapAt "multiline" . at "key3" . just_ . _String
+    actual1 = kv ^? mapAt "string" . mapAt "multiline" . valueAt "key1" . _Text
+    actual2 = kv ^? mapAt "string" . mapAt "multiline" . valueAt "key2" . _Text
+    actual3 = kv ^? mapAt "string" . mapAt "multiline" . valueAt "key3" . _Text
 
 testStringMultilineContinued :: Table -> Test
 testStringMultilineContinued kv =
@@ -113,9 +124,9 @@ testStringMultilineContinued kv =
     allEqual
     [actual1, actual2, actual3]
   where
-    actual1 = kv ^? mapAt "string" . mapAt "multiline" . mapAt "continued" . at "key1" . just_ . _String
-    actual2 = kv ^? mapAt "string" . mapAt "multiline" . mapAt "continued" . at "key2" . just_ . _String
-    actual3 = kv ^? mapAt "string" . mapAt "multiline" . mapAt "continued" . at "key3" . just_ . _String
+    actual1 = kv ^? mapAt "string" . mapAt "multiline" . mapAt "continued" . valueAt "key1" . _Text
+    actual2 = kv ^? mapAt "string" . mapAt "multiline" . mapAt "continued" . valueAt "key2" . _Text
+    actual3 = kv ^? mapAt "string" . mapAt "multiline" . mapAt "continued" . valueAt "key3" . _Text
 
 testArrayKey1 :: Table -> Test
 testArrayKey1 kv =
@@ -144,10 +155,10 @@ runTests kv = runTest . ($ kv) <$> tests
       ]
 
 readTomlFile :: String -> IO Table
-readTomlFile file = readFile file >>= parse >>= handleError
+readTomlFile file = Text.readFile file >>= parse >>= handleError
   where
     parse = pure . Toml.parse
-    handleError = either (error . show) pure
+    handleError = either (error . show) (pure . Toml.forgetTableAnns)
 
 main :: IO ()
 main = do
